e9cfaae7f54c8d5589a82139ae200919ecc34deb
Alejandra Landa <aleriquene@gmail.com>
2023-03-24 17:17:17 +0000

    finished

---
+++ app/controllers/currencies_controller.rb
@@ -0,0 +1,35 @@
+class CurrenciesController < ApplicationController
+  def first_currency
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+
+    @array_of_symbols = @symbols_hash.keys
+    render({:template => "currency_templates/step_one.html.erb"})
+  end
+  def second_currency
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+
+    @array_of_symbols = @symbols_hash.keys
+    @from_symbol = params.fetch("from_currency")
+    render({:template => "currency_templates/step_two.html.erb"})
+  end
+  def conversion_currency
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+
+    @array_of_symbols = @symbols_hash.keys
+    @from_symbol = params.fetch("from_currency")
+    @conversion = params.fetch("conversion")
+
+    @conversion_raw_data = open("https://api.exchangerate.host/convert?from=USD&to=EUR").read
+    @conversion_parsed_data = JSON.parse(@conversion_raw_data)
+    @conversion_rate = @conversion_parsed_data.fetch("info")
+    @final_conversion_rate = @conversion_rate.fetch("rate")
+    render({:template=>"currency_templates/conversion.html.erb"})
+  end
+
+end
